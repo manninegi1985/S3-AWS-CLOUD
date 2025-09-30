44cfe48b88407a0658f177b7accacef7c8b68ae3
manninegi1985 <negithecool@gmail.com>
2025-09-30 14:59:56 +0800

    S3 creation

---
+++ porvider.tf
@@ -0,0 +1,26 @@
+terraform {
+  cloud { 
+    
+    organization = "MANNINEGI" 
+
+    workspaces { 
+      name = "S3-AWS" 
+    } 
+  } 
+
+   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.54.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.6.2"
+    }
+}
+}
+
+provider "aws" {
+    region = "eu-west-1"
+}
+
